214dbff38e9ca22f7de1af20a043da7d6b9c4fcb
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-03-21 23:14:26 -0400

    Inicio proyecto-backend

---
+++ src/main/java/ucb/edu/bo/sis/ventaropa/VentaropaApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.sis.ventaropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaropaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaropaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/edu/bo/sis/ventaropa/VentaropaApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.sis.ventaropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaropaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
