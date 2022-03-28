af794c8e39c7a3a1eeb23e1f1d1c583f71197430
joelalex10 <joel.alejandro.quispe.cussi@gmail.com>
2022-03-27 22:38:59 -0400

    agregando campo tipo usuario a objeto de solitud AuthRequest

--- src/main/java/ucb/edu/bo/sis/ventaropa/dto/AuthRequest.java
+++ src/main/java/ucb/edu/bo/sis/ventaropa/dto/AuthRequest.java
@@ -6,12 +6,15 @@ public class AuthRequest implements Serializable {
     private static final long serialVersionUID = 5926468583005150707L;
     private String username;
     private String password;
+    private Integer tipoUsuarioId;
 
     public AuthRequest() {
     }
-    public AuthRequest(String username, String password) {
+
+    public AuthRequest(String username, String password, Integer tipoUsuarioId) {
         this.username = username;
         this.password = password;
+        this.tipoUsuarioId = tipoUsuarioId;
     }
 
     public String getUsername() {
@@ -30,11 +33,20 @@ public class AuthRequest implements Serializable {
         this.password = password;
     }
 
+    public Integer getTipoUsuarioId() {
+        return tipoUsuarioId;
+    }
+
+    public void setTipoUsuarioId(Integer tipoUsuarioId) {
+        this.tipoUsuarioId = tipoUsuarioId;
+    }
+
     @Override
     public String toString() {
         return "AuthRequest{" +
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", tipoUsuarioId=" + tipoUsuarioId +
                 '}';
     }
 }
