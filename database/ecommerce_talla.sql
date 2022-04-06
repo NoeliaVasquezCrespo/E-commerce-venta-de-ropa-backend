1ad3e9ac9232e2cdef04365fee600404486bea34
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-04-05 23:15:59 -0400

    Agregando datos de color y tallas

--- database/ecommerce_talla.sql
+++ database/ecommerce_talla.sql
@@ -1,12 +1,9 @@
-INSERT INTO talla (id, nombre_talla) VALUES (1,'S');
-INSERT INTO talla (id, nombre_talla) VALUES (2,'M');
-INSERT INTO talla (id, nombre_talla) VALUES (3,'L');
-INSERT INTO talla (id, nombre_talla) VALUES (4,'XL');
-INSERT INTO talla (id, nombre_talla) VALUES (5,'XXL');
+INSERT INTO talla (id, nombre_talla) VALUES (1,'XXS');
+INSERT INTO talla (id, nombre_talla) VALUES (2,'XS');
+INSERT INTO talla (id, nombre_talla) VALUES (3,'S');
+INSERT INTO talla (id, nombre_talla) VALUES (4,'M');
+INSERT INTO talla (id, nombre_talla) VALUES (5,'L');
+INSERT INTO talla (id, nombre_talla) VALUES (6,'XL');
+INSERT INTO talla (id, nombre_talla) VALUES (7,'XXL');
 
 
--- Talla-Producto---
-INSERT INTO talla_producto (talla_id, producto_id,stock,status) VALUES (1,1,50,1);
-INSERT INTO talla_producto (talla_id, producto_id,stock,status) VALUES (2,1,100,1);
-INSERT INTO talla_producto (talla_id, producto_id,stock,status) VALUES (3,1,45,1);
-INSERT INTO talla_producto (talla_id, producto_id,stock,status) VALUES (4,1,20,1);
