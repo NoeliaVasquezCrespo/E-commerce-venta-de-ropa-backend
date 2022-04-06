8c1eca0a915de689d79b0450ab50b7ba0da1bfa9
joelalex10 <joel.alejandro.quispe.cussi@gmail.com>
2022-04-06 08:42:51 -0400

    logica para subir imagenes

--- database/ecommerce_productos.sql
+++ database/ecommerce_productos.sql
@@ -1,6 +1,8 @@
-INSERT INTO producto (id, nombre_producto,precio,status,proveedor_id, descripcion) VALUES (1, 'Tenis', 141.68, 1, 1, 'Zapatillas para hombre y mujer de todo color');
-INSERT INTO producto (id, nombre_producto,precio,status,proveedor_id, descripcion) VALUES (2, 'Pantalones', 89.5, 1, 2, 'Pantalones Vaqueros Para mujer');
+INSERT INTO producto (id, codigo_producto, nombre_producto, descripcion, stock, precio, color_id, talla_id, administrador_id, status) VALUES (25, 'SWAE345', 'Camiseta Para Mujer Nicor', 'Camisetas de mujer', 20, 135.00, 1, 2, 1, 1);
+INSERT INTO producto (id, codigo_producto, nombre_producto, descripcion, stock, precio, color_id, talla_id, administrador_id, status) VALUES (26, 'SER214', 'Camista para mujer tinsa', 'Camisetas de mujer', 54, 135.00, 1, 3, 1, 1);
 
+INSERT INTO fotos_producto (id, producto_id, foto) VALUES (11, 25, 'productImage/fwav74entb20224683346User25.png');
+INSERT INTO fotos_producto (id, producto_id, foto) VALUES (12, 26, 'productImage/z7u55t8zve20224683552User26.png');
 
 -- Consulta a base de datos
 
