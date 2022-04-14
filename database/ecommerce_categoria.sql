b162209ed77f124f4a1e4bd34af36819d84c2cf5
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-04-13 21:01:57 -0400

    Agregando categorias para la base de datos

--- database/ecommerce_categoria.sql
+++ database/ecommerce_categoria.sql
@@ -1,18 +1,23 @@
-INSERT INTO categoria (id, categoria, status) VALUES (1, 'Pantalones', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (2, 'Zapatos', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (3, 'Blusas', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (4, 'Guantes', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (5, 'Camisas', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (6, 'Poleras', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (7, 'Vestidos', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (8, 'Abrigos', 1);
-INSERT INTO categoria (id, categoria, status) VALUES (9, 'Tenis', 1);
-
-
 
 -- Producto-Categoria--
 #INSERT INTO producto_categoria (categoria_id,producto_id) VALUES (9,1);
 #INSERT INTO producto_categoria (categoria_id,producto_id) VALUES (1,2);
 
 
+INSERT INTO categoria (id, categoria, status) VALUES (1, 'Abrigo', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (2, 'Blusa', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (3, 'Camisa', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (4, 'Camiseta', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (5, 'Chaqueta', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (6, 'Chaleco', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (7, 'Falda', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (8, 'Impermeable', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (9, 'Pantalón', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (10, 'Pantalón corto', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (11, 'Pantalón vaquero', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (12, 'Pijama', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (13, 'Polera', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (14, 'Saco', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (15, 'Sudadera', 1);
+INSERT INTO categoria (id, categoria, status) VALUES (16, 'Vestido', 1);
 
