e607d18b8e6450f7653bb2b4d3328a9f72ab79df
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-05-20 22:42:35 -0400

    Agregando datos para la base de datos

---
+++ database/ecommerce_oferta.sql
@@ -0,0 +1,5 @@
+INSERT INTO oferta (id,fecha_inicio, fecha_fin,monto_descuento,status) VALUES (1,"2022-05-20","2022-05-27",100,1);
+INSERT INTO oferta (id,fecha_inicio, fecha_fin,monto_descuento,status) VALUES (2,"2022-05-27","2022-06-01",200,1);
+INSERT INTO oferta (id,fecha_inicio, fecha_fin,monto_descuento,status) VALUES (3,"2022-06-01","2022-06-06",35.5,1);
+INSERT INTO oferta (id,fecha_inicio, fecha_fin,monto_descuento,status) VALUES (4,"2022-06-07","2022-06-10",71.5,1);
+INSERT INTO oferta (id,fecha_inicio, fecha_fin,monto_descuento,status) VALUES (5,"2022-06-15","2022-06-21",16,1);
